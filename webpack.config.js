c83354ad9ec18ed9fd95b1603eaf067897b73fab
jevoncode <jevoncode@gmail.com>
2016-11-28 00:05:12 +0800

    学习插件的使用，使用第一个webpack的插件 new webpack.BannerPlugin添加版权信息

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
- 
+var webpack = require('webpack');
+
 module.exports = {
 	 devtool: 'source-map',//配置生成Source Maps，选择合适的选项
      entry: __dirname + "/app/main.js",  //唯一入口文件
@@ -22,6 +23,9 @@ module.exports = {
      		}
      	]
      },
+     plugins: [
+     	new webpack.BannerPlugin("Copyright jevoncode") //在这个plugins数组里new一个就可以了
+     ],
      devServer:{
      	contentBase: "./public", //本地服务器所加载的页面所在的目录
      	colors: true, //终端中输出结果为彩色
